25c5fa6cc6ff25a4510adac6a42a4a72f88b5b88
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-09 13:22:29 +0530

    Added:Switch case functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,20 @@
 echo "Welcome to Employee wage computation prgram"
-isPresent=1
+
 wageperhr=20
 ispartTime=1
 isfullTime=2
-randomCheck=$(( RANDOM%2 ))
-if [ $isPresent -eq $randomCheck ]
-then
-        empcheck=$(( RANDOM%3 ))
-           if [ $empcheck -eq $ispartTime ]
-           then
-              emphrs=4
-           elif [ $empcheck -eq $isfullTime ]
-           then
-              emphrs=8
 
-else
-        emphrs=0
-fi
-fi
-          salary=$(( emphrs*wageperhr ))
+        empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+             $ispartTime)
+                   emphrs=4;;
+             $isfullTime)
+                   emphrs=8;;
+            *)
+        emphrs=0;;
+      esac
+
+          salary=$(( $emphrs*$wageperhr ))
           echo "$salary"
 
 
